b731663dcfcff6d18f3d724f55320e96fce06fc1
Ahmed Munna <munnaa329@gmail.com>
2023-11-25 13:03:48 +0600

    updated db config

--- knexfile.ts
+++ knexfile.ts
@@ -1,18 +1,17 @@
-import 'dotenv/config';
+import { config } from 'dotenv';
 import { Knex } from 'knex';
-import { knexSnakeCaseMappers } from 'objection';
+
+config();
 
 module.exports = {
   client: 'pg',
-  connection: 'postgres://db:db@localhost:5432/db',
-  // connection: process.env.DB_URL,
+  connection: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
   migrations: {
     directory: './src/database/migrations',
     stub: './src/database/migration.stub',
   },
   seeds: {
     directory: './src/database/seeds',
-    stub: './src/database/seed.stub'
+    stub: './src/database/seed.stub',
   },
-  // ...knexSnakeCaseMappers()
 } as Knex.Config;
