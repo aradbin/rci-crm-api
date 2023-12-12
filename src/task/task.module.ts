0e6ded556a3fddd4b3de9bfb9cf733087952e78a
Arad Bin <aradbin@gmail.com>
2023-12-13 00:10:54 +0600

    unused clear

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
-import { TaskModel } from './task.model';
 import { TaskService } from './task.service';
 
 @Module({
   controllers: [TaskController],
   providers: [TaskService],
 })
-export class TaskModule {}
+export class TaskModule { }
