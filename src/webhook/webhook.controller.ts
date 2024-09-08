5fd1955a304d94d2d459cb9ae7b83034df9505ce
Arad Bin <aradbin@gmail.com>
2024-09-09 02:02:15 +0600

    webhook public

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -1,16 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { Public } from 'src/auth/public.decorators';
 import { WebhookService } from './webhook.service';
 
 @Controller('webhook')
 export class WebhookController {
   constructor(private readonly webhookService: WebhookService) {}
 
+  @Public()
   @Post('whatsapp')
   async whatsapp(@Body() payload: any) {
     console.log('webhook.controller',payload);
     return await this.webhookService.webhook('whatsapp', payload);
   }
 
+  @Public()
   @Post('email')
   async email(@Body() payload: any) {
     return await this.webhookService.webhook('email', payload);
