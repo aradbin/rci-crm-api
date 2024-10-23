39d913de755c9d04a46b0d8b8e6cde26ded5f31e
Arad Bin <aradbin@gmail.com>
2024-10-24 03:22:53 +0600

    knexfile update

--- knexfile.ts
+++ knexfile.ts
@@ -5,7 +5,9 @@ config();
 
 module.exports = {
   client: 'pg',
-  connection: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}?sslmode=require`,
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+  },
   pool: {
     min: 0,
     max: 10,
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -51,7 +51,9 @@ const providers = [
     useFactory: async () => {
       const knex = Knex({
         client: 'pg',
-        connection: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}?sslmode=require`,
+        connection: {
+          connectionString: process.env.DATABASE_URL,
+        },
       });
 
       Model.knex(knex);
