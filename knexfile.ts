74c6bde6bf9c50823ebf565d6b7a046d55e96fe3
Arad bin <arad.siddiqui@upaybd.com>
2024-10-24 13:30:04 +0600

    knexfile update

--- knexfile.ts
+++ knexfile.ts
@@ -2,7 +2,9 @@ import { config } from 'dotenv';
 import { Knex } from 'knex';
 
 config();
-console.log(process.env.DATABASE_URL);
+console.log(
+  `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
+);
 module.exports = {
   client: 'pg',
   connection: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
